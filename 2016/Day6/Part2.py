4c8c3ceb5cc35abe8a0d1217a0b4a0bd4b79177b
Yash Mangal <yashmangal10@gmail.com>
2022-01-20 02:23:47 +0530

    Added 2016 Day6

---
+++ 2016/Day6/Part2.py
@@ -0,0 +1,15 @@
+with open("2016\Day6\input.txt") as input:
+    puzzleinput = input.read()
+    puzzleinput = puzzleinput.split("\n")
+
+code = ""
+
+for i in range(8):
+    countdict = {}
+    for char in [j[i] for j in puzzleinput]:
+        countdict.setdefault(char, 0)
+        countdict[char] += 1
+    sorted_dict = sorted(countdict.items(), key=lambda x: (-x[1], x[0]))
+    code += str(sorted_dict[-1][0])
+
+print(code)
